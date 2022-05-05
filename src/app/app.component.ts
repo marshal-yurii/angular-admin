84222dff457939bda15f11565f890d7a4f19f4b6
yuriimarshal <y.marshal@abtollc.com>
2022-05-05 20:05:15 +0300

    breadcrumbs init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-admin';
+
+  breadcrumbs: string[] = [];
+
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.breadcrumbs = [
+      'dashboard',
+      'users',
+      'user',
+    ];
+  }
+
+  redirectTo(breadcrumb: string): void {
+    this.router.navigateByUrl(breadcrumb);
+  }
 }
