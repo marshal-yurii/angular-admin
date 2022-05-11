213d74082d9fc59b7a3fd43895e37451ec904a51
yuriimarshal <y.marshal@abtollc.com>
2022-05-11 22:48:42 +0300

    [ANG 2] create system structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./features/dashboard/components/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./features/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./features/settings/settings.module').then((m) => m.SettingsModule),
+  },
+  {path: '**', redirectTo: '/error/404'},
+];
+
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  scrollPositionRestoration: 'top',
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, routerOptions),
+  ],
+  exports: [
+    RouterModule,
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
